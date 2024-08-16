06f8df75345c59bfa40daf58c359f2099e24f984
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-08-16 23:28:16 +0400

    finish section 3

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button(props) {
   return (
-    <button className='btn'>{props.text}</button>
+    <button className='btn' style={{padding:props.padding, width:props.width, marginTop:"50px"}}>{props.text}</button>
   )
 }
 
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 function Form() {
   return (
-    <form >
-
+    <form className='form'>
+        <div className='formText'>
+            <h1>Get a free Keystroke</h1>
+            <h1>quote now</h1>
+            <h5>Name</h5>
+            <input type="text" placeholder='John Smith' required/>
+            <h5>Email</h5>
+            <input type="email" placeholder='example@mail.com' required/>
+            <h5>Phone</h5>
+            <input type="number" placeholder='+123456789' required/>
+            <Button text="Get Free Quote" padding="20px" width="100%"/>
+        </div>
     </form>
   )
 }
